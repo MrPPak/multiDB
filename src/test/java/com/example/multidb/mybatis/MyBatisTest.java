47ba4e15ad3c8962bcb1f60ccc82e118686108e3
MrPPak <matht1115@naver.com>
2022-03-02 16:04:37 +0900

    refactor: MyBatisTest 리팩토링

--- src/test/java/com/example/multidb/mybatis/MyBatisTest.java
+++ src/test/java/com/example/multidb/mybatis/MyBatisTest.java
@@ -4,11 +4,15 @@ import com.example.multidb.domain.femarket.PdtStatic;
 import com.example.multidb.domain.fenote.FePoint;
 import com.example.multidb.mapper.femarket.PdtStaticMapper;
 import com.example.multidb.mapper.fenote.FePointMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 public class MyBatisTest {
@@ -20,10 +24,15 @@ public class MyBatisTest {
 
     @Test
     void multiMyBatisTest() {
+        // when
         List<FePoint> fepoints = fePointMapper.findAll();
         List<PdtStatic> pdtStatics = pdtStaticMapper.findAll();
-        System.out.println("fepoints.size() = " + fepoints.size());
-        System.out.println("pdtStatics.size() = " + pdtStatics.size());
+
+        // then
+        assertAll(() -> {
+            assertThat(fepoints.size()).isGreaterThan(0);
+            assertThat(pdtStatics.size()).isGreaterThan(0);
+        });
     }
 
 }
